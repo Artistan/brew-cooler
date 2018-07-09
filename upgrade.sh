341022deb8920f90803d902715e28fa9cc1f9c7f
Charles Peterson <cpeterson@brokerbin.com>
2018-07-09 08:33:06 -0500

    upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# run as user... get configs from rc files.
+# not caring if this fails at all, just need to know the env for brew
+source ~/.zshrc 2> /dev/null
+source ~/.bashrc 2> /dev/null
+
+brew cleanup
+brew doctor
+
+brew update
+brew upgrade
+pip install --upgrade pip setuptools
+
+brew bundle
+
+brew cleanup
+brew doctor
+
+brew bundle --force dump
+
+
+vagrant plugin repair
+vagrant plugin update
