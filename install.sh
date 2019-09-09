f613f78d6a6a08bdfcd74740fec9b73465e4a2a2
Charles Peterson <artistan@gmail.com>
2019-09-09 10:35:58 -0500

    install + vagrant

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew bundle
+
+vagrant plugin install vagrant-host-shell
+vagrant plugin install vagrant-bindfs
+vagrant plugin install vagrant-hostsupdater
+vagrant plugin install vagrant-vbguest
